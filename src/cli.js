9dad40830948acb3d32690d9d2dc9338cf7893b1
Olha Hlushko <oli4ka14@gmail.com>
2021-07-20 12:12:33 +0300

    modified files

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readline from 'readline-sync';
+
 const hello = () => {
-	const name = readline.question('May I have your name? ');
-	console.log(`Hello, ${name}!`);
+  const name = readline.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
 export default hello;
