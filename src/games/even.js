a7e42bf01d988118e6f2fc040beecb1586eb943f
Olha Hlushko <oli4ka14@gmail.com>
2021-08-30 20:25:31 +0300

    modified even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,9 +1,15 @@
+import game from '../index.js';
+import random from '../random.js';
+
 const phrase = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (number) => number % 2 === 0;
 const gameQuestion = () => {
-  const num = Math.floor(Math.random() * 101);
+  const num = random();
+  const question = `Question: ${num}\nYour answer: `;
   const correctAnswer = isEven(num) ? 'yes' : 'no';
 
-  return { num, correctAnswer };
+  return [question, correctAnswer];
 };
-export default { phrase, gameQuestion };
+export default () => {
+  game(gameQuestion, phrase);
+};
